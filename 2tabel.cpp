0f7f6674a358fc83f9edf60251c087a09bc1e701
veerthakur1272 <veerbhansinghthakur35@gmail.com>
2024-11-20 19:04:00 +0530

    addit agin

---
+++ 2tabel.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for (int i=2; i<=5;++i){
+        for (int j=1;j<=10;++j)
+            cout<<i<<"*"<<j<<"="<<i*j<<endl<<endl;
+    }
+
+}
+
---
+++ star20nov.cpp
@@ -0,0 +1,26 @@
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+   for(int r=1;r<=5;++r)
+   {
+       for(int c=1;c<=r;++c){
+       cout<<"*";
+       }
+       cout<<endl;
+   }
+}
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+   for(int r=1;r<=5;++r)
+   {
+       for(int c=1;c<=r;++c){
+       cout<<"*";
+       }
+       cout<<endl;
+   }
+}
