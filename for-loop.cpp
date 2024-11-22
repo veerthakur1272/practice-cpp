0104640cd7bb8339c8655a5adb7a8bc8bf886a36
veerthakur1272 <veerbhansinghthakur35@gmail.com>
2024-11-22 19:02:16 +0530

    addit agin

--- for-loop.cpp
+++ for-loop.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 int main()
@@ -16,6 +17,49 @@ int main()
                 cout<<"*";
              else
                 cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+*/
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int r=1;r<=5;++r)
+    {
+        for(int c=1;c<=9;++c)
+        {
+            if(c>=6-r && c<=4+r)
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int r=1;r<=5;++r)
+    {
+        for(int c=1;c<=9;++c)
+        {
+            if(c>=r && c<=10-r)
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
         }
         cout<<endl;
     }
