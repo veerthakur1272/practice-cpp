fee949df1aadb9bc58de83560239f4bef948dc97
veerthakur1272 <veerbhansinghthakur35@gmail.com>
2024-12-25 19:48:28 +0530

    addit agin

--- reversofstring.cpp
+++ reversofstring.cpp
@@ -22,7 +22,7 @@ int main()
      cout<<str;
 }
 */
-
+/*
 #include<iostream>
 using namespace std;
 void pal(string &str)
@@ -47,5 +47,64 @@ int main()
     if(pal(str))
     {
         cout<<"palindrome";
+    }
+}
+*/
+
+#include<iostream>
+using namespace std;
+void bp(string &str)
+{
+    int circle=0;
+    int square=0;
+    int curly=0;
+
+    for(int i=0;i<str.length();++i)
+    {
+        char ch=str[i];
+        if (ch=='(') circle++;
+        else if (ch=='[') square++;
+        else if (ch=='{') curly++;
+
+        else if (ch==')'){
+            if (circle==0)
+            {
+                return false;
+            }
+            circle--;
+        }
+        else if (ch=='}'){
+            if (curly==0)
+            {
+                return false;
+            }
+            curly-;
+        }
+
+        else if(ch==']'){
+            if (square==0)
+            {
+                return false;
+            }
+            square-;
+        }
+    }
+
+    if (circle==0 && square==0 && curly==0)
+    {
+        return false;
+    }
+    return false;
+
+}
+int main ()
+{
+    string stream="([{}])";
+    if(bp(str))
+    {
+        cout<<"balanced";
+    }
+    else{
+        count<<"unbalanced";
     }
 }
